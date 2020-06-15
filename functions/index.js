99b68016ba9872c6cc8cf6ba4e805b89499e4efc
Leopoldo Brines <leopoldo.brines@mercadolibre.com>
2020-06-15 16:44:15 -0300

    Add inscribe player function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const config = functions.config().env
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const SteamAPI = require('steamapi')
+const steam = new SteamAPI(config.steam.apikey)
+
+exports.inscribePlayer = functions.https.onCall((data, context) => {
+	console.log('Context: ', context)
+	console.log('Data: ', data)
+
+	var player = {}
+
+	return steam
+		.resolve(data.steamURL)
+		.then((steamid64) => steam.getUserBans(steamid64))
+		.then((bans) => {
+			player.bans = bans
+			return steam.getUserOwnedGames(player.steamid)
+		})
+		.then((games) => {
+			player.games = games
+			console.log(player)
+			return player
+		})
+})
